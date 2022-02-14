8d94e873e17f5ec60e5359ecadb435dbf5c8f2ea
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-02-14 19:36:23 +0100

    Pagina funciona, web app desplegada en el puerto 8433

---
+++ FoodAndUApp/src/main/java/com/example/foodanduapp/FoodAndUAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodanduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAndUAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAndUAppApplication.class, args);
+	}
+
+}
--- FoodAndUApp/src/test/java/com/example/foodanduapp/FoodAndUAppApplicationTests.java
+++ FoodAndUApp/src/test/java/com/example/foodanduapp/FoodAndUAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.foodanduapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FoodAndUAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
