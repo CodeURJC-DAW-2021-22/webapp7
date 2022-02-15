8d701be83cc5a5e28b177cef0b82b4af98e860ce
Hugo <hugo_cg6@outlook.com>
2022-02-15 19:23:01 +0100

    Backend bien estructurado

---
+++ backend/src/main/java/com/example/demo/model/ShoppingList.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+public class ShoppingList {
+    Map<String, Integer> numberofIntredients;
+    ArrayList<String> list;
+
+
+    public ShoppingList(Recipe[] weeklyPlan) {
+        for (Recipe r : weeklyPlan) {
+            for (String s : r.getIngredients()) {
+                Integer n = numberofIntredients.get(s);
+                if (n == null)
+                    numberofIntredients.put(s, 1);
+                else
+                    numberofIntredients.put(s, n + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> e : numberofIntredients.entrySet()) {
+            String s = e.getKey() + " -> " + String.valueOf(e.getValue());
+            list.add(s);
+        }
+    }
+}
