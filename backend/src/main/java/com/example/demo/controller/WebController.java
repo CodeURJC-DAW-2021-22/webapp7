e0e04b8362b23db3cb0ec4e25930cbd5e470fe14
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-02-17 12:12:39 +0100

    Controladores de navegacion añadidos

--- backend/src/main/java/com/example/demo/controller/WebController.java
+++ backend/src/main/java/com/example/demo/controller/WebController.java
@@ -13,7 +13,30 @@ public class WebController {
         return "index.html";
     }
 
-
     @GetMapping("/Recipes")
     public String getRecipes(){return "recipe.html";}
+
+    @GetMapping("/Recipe")
+    public String getRecipe(){return "details.html";}
+
+    @GetMapping("/LogIn")
+    public String getLogin(){return "login.html";}
+
+    @GetMapping("/YourMenu")
+    public String getMenu_Activo(){return "Menu_Activo.html";}
+
+    @GetMapping("/StoredRecipes")
+    public String getStored_Recipes(){return "Stored_Recipes.html";}
+
+    @GetMapping("/User")
+    public String getUser(){return "user.html";}
+
+    @GetMapping("/AboutUs")
+    public String getAboutUs(){return "AboutUs.html";}
+
+    @GetMapping("/StoredDiets")
+    public String getStoredDiets(){return "DropDown.html";}
+
+    @GetMapping("/AdminProfile")
+    public String getAdminProfile(){return "Admin.html";}
 }
