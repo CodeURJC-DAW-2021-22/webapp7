629dc166a21248d2dbd387bc0487311892b35ec7
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-04-03 16:56:27 +0200

    Error de token + configuracion de seguridad api rest

--- backend/src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         // Login form
-        http.formLogin().loginPage("/LogIn");
+        http.formLogin().loginPage("/processFormLogIn");
         http.formLogin().usernameParameter("name");
         http.formLogin().passwordParameter("password");
         http.formLogin().defaultSuccessUrl("/");
