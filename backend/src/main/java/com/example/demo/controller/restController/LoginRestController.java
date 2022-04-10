33925186f4c2b3a28999e40822494f6eae196dcb
CalejandroURJC <c.alejandro.2019@alumnos.urjc.es>
2022-04-10 13:29:27 +0200

    Solucion login

--- backend/src/main/java/com/example/demo/controller/restController/LoginRestController.java
+++ backend/src/main/java/com/example/demo/controller/restController/LoginRestController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class LoginRestController {
 
     @Autowired
