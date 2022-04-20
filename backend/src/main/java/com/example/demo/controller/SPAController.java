037904b84846e902019819e2540bbd817f1f211e
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-04-20 14:59:51 +0200

    correccion error spa controller

--- backend/src/main/java/com/example/demo/controller/SPAController.java
+++ backend/src/main/java/com/example/demo/controller/SPAController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    @GetMapping({"/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}"})
+    @GetMapping({"/new/**", "/{path:new[^\\.]*}"})
     public String redirect() {
         return "forward:/new/index.html";
     }
 }
+
+//este controlador no ejecutaba bien con la versión de las transparencias asi que hemos borrado la primera parte del mapping para que no de error.
+//hay que probarlo cuando tengamos avanzada la parte de angular
