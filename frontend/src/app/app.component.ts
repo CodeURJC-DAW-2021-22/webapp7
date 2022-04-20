2f64f30bd6c2b1f836bc4f72438a9339f360fd66
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-04-20 17:29:25 +0200

    inicio proyecto con css pero mal linkeado app-root

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:['../assets/css/styles.css',
+  '../assets/css/bootstrap.min.css',
+  '../assets/css/donwload.css',
+  '../assets/css/font-awesome.min.css',
+  '../assets/css/tables.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "Food & U"
+  logged = true;
+  admin = true;
+
+
+  isLogged(logged:boolean){
+    this.logged=logged;
+  }
+
+  isAdmin(admin:boolean){
+    this.admin=admin
+  }
 }
