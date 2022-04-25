33a5ba091e7bf2825e78962a865327a3dab0037c
Hugo <hugo_cg6@outlook.com>
2022-04-26 01:26:18 +0200

    Resto de clases nuevas (revisar)

---
+++ frontend/src/app/Class/Menu/menu.ts
@@ -0,0 +1,7 @@
+import { Recipes } from "../Recipes/recipes";
+
+export class Menu {
+    id: Number;
+    name: String;
+    weeklyPlan: Recipes[]
+}
---
+++ frontend/src/app/Class/Users/users.ts
@@ -0,0 +1,15 @@
+import { Diet } from "../Diet/diet";
+import { Menu } from "../Menu/menu";
+import { Recipes } from "../Recipes/recipes";
+
+export class Users {
+    id: Number;
+    name: String;
+    password: String;
+    mail: String;
+    admin: boolean;
+
+    activeMenu: Menu;
+    storedRecipes: Recipes[];
+    soredDiets: Diet[];
+}
