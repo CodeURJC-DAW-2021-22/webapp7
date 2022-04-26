bb8ecad32f6677f6934a4c2bbdeb9a775091db08
Larrivey <r.marques.2018@alumnos.urjc.es>
2022-04-26 14:20:15 +0200

    Intento de sacar todos los nombres en reccipesall

--- frontend/src/app/Templates/Recipe/recipeall/recipeall.component.ts
+++ frontend/src/app/Templates/Recipe/recipeall/recipeall.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Recipes } from 'src/app/Class/Recipes/recipes';
+import { RecipesService } from 'src/app/services/Recipes/recipes.service';
 
 @Component({
   selector: 'app-recipeall',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeallComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  recipes:Recipes[];
+
+  constructor(private router:Router, private service:RecipesService) {
+    this.service.getAllRecipes().subscribe(
+      recipes => this.recipes = recipes,
+      error => console.log(error)
+    );
   }
 
-}
+  ngOnInit(): void {
+    }
+
+  recipeID(): void{
+      this.router.navigate(['/']);
+    }
+
+  }
+
